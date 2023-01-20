cb5803d354eaf7327aeb1a17ce45b2c0c367edc3
Valentin Mahu <mahu31valentin@gmail.com>
2023-01-20 11:04:25 +0200

    last modification

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger, .menu').toggleClass('active');
 		$('.page').toggleClass('page_active');
-			$('body').toggleClass('body_active');
+		$('body').toggleClass('body_active');
 	});
 });
 
